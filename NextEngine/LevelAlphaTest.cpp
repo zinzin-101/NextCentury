0950a047691fd29936cc32ffc214926024789dd4
zinzin-101 <mr.punthep@gmail.com>
2025-04-09 17:12:17 +0700

    remove jumping from player

--- NextEngine/LevelAlphaTest.cpp
+++ NextEngine/LevelAlphaTest.cpp
@@ -151,9 +151,8 @@ void LevelAlphaTest::handleKey(InputManager& input) {
     processKeyBuffer(input, SDLK_LSHIFT);
 
     // handle event here
-    if (input.getButton(SDLK_SPACE)) player->jump();
-    if (input.getButton(SDLK_a)) player->move(glm::vec2(-1, 0));
-    if (input.getButton(SDLK_d)) player->move(glm::vec2(1, 0));
+    if (input.getButton(SDLK_a) && !input.getButton(SDLK_d)) player->move(glm::vec2(-1, 0));
+    if (input.getButton(SDLK_d) && !input.getButton(SDLK_a)) player->move(glm::vec2(1, 0));
     if (input.getButtonDown(SDLK_j)) player->parryAttack();
     if (input.getMouseButtonDown(SDL_BUTTON_RIGHT)) player->parryAttack();
 
