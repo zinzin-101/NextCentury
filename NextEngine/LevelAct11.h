26109095d622e744b5824e5cfb7bbdf69122ddad
nmmn4947 <nmmn4947@gmail.com>
2025-06-03 22:53:00 +0700

    Act11Setup

---
+++ NextEngine/LevelAct11.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Level.h"
+#include "ObjectHeader.h"
+
+class LevelAct11 : public Level {
+private:
+    list<DrawableObject*> objectsList;
+    list<DrawableObject*> UIobjectsList;
+    PlayerObject* player = nullptr;
+    IngameUI* UIobject;
+    
+    InteractableObject* shelf;
+    InteractableObject* medicine;
+    ProtagThoughts* p1;
+
+    bool isStop;
+    FadeBlack* fb;
+    bool end = false;
+    float timefade = 1.0f;
+
+public:
+    virtual void levelLoad();
+    virtual void levelInit();
+    virtual void levelUpdate();
+    virtual void levelDraw();
+    virtual void levelFree();
+    virtual void levelUnload();
+
+    virtual void handleKey(InputManager& input);
+};
